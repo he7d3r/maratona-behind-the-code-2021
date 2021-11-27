2e04697e6efa19cb80bac93809672fb9b202e957
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2021-11-27 09:20:49 -0300

    Test and validate co2 levels

--- 2-quanam/solução/sensor_alert/src/main.py
+++ 2-quanam/solução/sensor_alert/src/main.py
@@ -1,3 +1,23 @@
+def get_acceptable_co2_levels():
+    return {
+        "activity-room": {"min": None, "max": 500},
+        "refectory": {"min": None, "max": 400},
+        "room-1": {"min": None, "max": 300},
+        "bathroom-main": {"min": None, "max": 500},
+        "garden": {"min": None, "max": 500},
+    }
+
+
 def main(sensor_data):
     result = {"alerts": []}
+    room = sensor_data["room"]
+    acceptable_co2_levels = get_acceptable_co2_levels()
+    acceptable_range = acceptable_co2_levels[room]
+    co2_level = sensor_data["values"]["co2"]
+
+    if (
+        acceptable_range["min"] is not None and co2_level < acceptable_range["min"]
+    ) or (acceptable_range["max"] is not None and co2_level > acceptable_range["max"]):
+        result["alerts"].append("co2")
+
     return result
--- 2-quanam/solução/sensor_alert/tests/test_main.py
+++ 2-quanam/solução/sensor_alert/tests/test_main.py
@@ -1,21 +1,34 @@
-from src.main import main
+from src.main import main, get_acceptable_co2_levels
 
 
-def test_main_has_alerts():
-    result = main({})
-    assert "alerts" in result
-
-
-def test_main_accepts_json_fields():
+def test_main_validates_co2():
     data = {
         "room": "bathroom-main",
         "values": {
-            "co2": 400,
-            "temperature": 22,
-            "humidity": 70,
-            "sound": 30,
-            "illumination": 150,
+            "co2": 0,
+            "temperature": 0,
+            "humidity": 0,
+            "sound": 0,
+            "illumination": 0,
         },
     }
     result = main(data)
     assert "alerts" in result
+    assert "co2" not in result["alerts"]
+
+    data["values"]["co2"] = 500
+    result = main(data)
+    assert "co2" not in result["alerts"]
+
+    data["values"]["co2"] = 501
+    result = main(data)
+    assert "co2" in result["alerts"]
+
+
+def test_co2_settings():
+    result = get_acceptable_co2_levels()
+    keys = ["activity-room", "refectory", "room-1", "bathroom-main", "garden"]
+    for key in keys:
+        assert key in result
+        assert "min" in result[key]
+        assert "max" in result[key]

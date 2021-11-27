26310a509ef8c929a77d21e0fe2207f7fadfd438
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2021-11-27 10:20:36 -0300

    Refactor, test and validate temperature levels

--- 2-quanam/solução/sensor_alert/src/main.py
+++ 2-quanam/solução/sensor_alert/src/main.py
@@ -7,17 +7,29 @@ def get_acceptable_co2_levels():
         "garden": {"min": None, "max": 500},
     }
 
+def get_acceptable_temperature_levels():
+    return {
+        "activity-room": {"min": 19, "max": 22},
+        "refectory": {"min": 20, "max": 23},
+        "room-1": {"min": 21, "max": 23},
+        "bathroom-main": {"min": 22, "max": 25},
+        "garden": {"min": 15, "max": 22},
+    }
 
 def main(sensor_data):
     result = {"alerts": []}
     room = sensor_data["room"]
-    acceptable_co2_levels = get_acceptable_co2_levels()
-    acceptable_range = acceptable_co2_levels[room]
-    co2_level = sensor_data["values"]["co2"]
+    acceptable_levels = {
+        "co2": get_acceptable_co2_levels(),
+        "temperature": get_acceptable_temperature_levels()
+    }
+    for sensor_name, levels in acceptable_levels.items():
+        acceptable = levels[room]
+        sensor_level = sensor_data["values"][sensor_name]
 
-    if (
-        acceptable_range["min"] is not None and co2_level < acceptable_range["min"]
-    ) or (acceptable_range["max"] is not None and co2_level > acceptable_range["max"]):
-        result["alerts"].append("co2")
+        if (
+            acceptable["min"] is not None and sensor_level < acceptable["min"]
+        ) or (acceptable["max"] is not None and sensor_level > acceptable["max"]):
+            result["alerts"].append(sensor_name)
 
     return result
--- 2-quanam/solução/sensor_alert/tests/test_main.py
+++ 2-quanam/solução/sensor_alert/tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import main, get_acceptable_co2_levels
+from src.main import main, get_acceptable_co2_levels, get_acceptable_temperature_levels
 
 
 def test_main_validates_co2():
@@ -32,3 +32,40 @@ def test_co2_settings():
         assert key in result
         assert "min" in result[key]
         assert "max" in result[key]
+
+
+def test_temperature_settings():
+    result = get_acceptable_temperature_levels()
+    keys = ["activity-room", "refectory", "room-1", "bathroom-main", "garden"]
+    for key in keys:
+        assert key in result
+        assert "min" in result[key]
+        assert "max" in result[key]
+
+
+def test_main_validates_temperature():
+    data = {
+        "room": "refectory",
+        "values": {
+            "co2": 0,
+            "temperature": 19,
+            "humidity": 0,
+            "sound": 0,
+            "illumination": 0,
+        },
+    }
+    result = main(data)
+    assert "alerts" in result
+    assert "temperature" in result["alerts"]
+
+    data["values"]["temperature"] = 20
+    result = main(data)
+    assert "temperature" not in result["alerts"]
+
+    data["values"]["temperature"] = 23
+    result = main(data)
+    assert "temperature" not in result["alerts"]
+
+    data["values"]["temperature"] = 24
+    result = main(data)
+    assert "temperature" in result["alerts"]
